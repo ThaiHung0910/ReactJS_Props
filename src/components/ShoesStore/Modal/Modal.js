f0cccbc9d7199a74fbe7dc80c6052cd213355492
ThaiHung <thaihung147741@gmail.com>
2024-02-23 22:12:45 +0700

    commit

--- src/components/ShoesStore/Modal/Modal.js
+++ src/components/ShoesStore/Modal/Modal.js
@@ -1,28 +1,25 @@
 /* eslint-disable no-lone-blocks */
 import React, { Component } from "react";
-import style from './css/Modal.module.css'
+import style from "./css/Modal.module.css";
 
 let showModal = () => {
-  let modalId = document.getElementById("modalId");
-  modalId.classList.add("show");
-  modalId.style.display = "block";
-  modalId.style.backgroundColor = "rgba(0, 0, 0, 0.75)";
+    let modalId = document.getElementById("modalId");
+    modalId.classList.add("show");
+    modalId.style.display = "block";
+    modalId.style.backgroundColor = "rgba(0, 0, 0, 0.75)";
 
-  window.onclick = event => {
-    if (event.target === modalId) {
-      modalId.style.display = "none";
-    }
-  }
-},
-closeModal = () => {
-  let modalId = document.getElementById("modalId");
-  modalId.classList.remove("show");
-  modalId.style.display = "none";
-  modalId.style.backgroundColor = 'none'
-  
-}
-
-
+    window.onclick = (event) => {
+      if (event.target === modalId) {
+        modalId.style.display = "none";
+      }
+    };
+  },
+  closeModal = () => {
+    let modalId = document.getElementById("modalId");
+    modalId.classList.remove("show");
+    modalId.style.display = "none";
+    modalId.style.backgroundColor = "none";
+  };
 
 export default class Modal2 extends Component {
   render() {
@@ -31,8 +28,6 @@ export default class Modal2 extends Component {
 
     return (
       <div>
-        
-
         <div className="modal fade" id="modalId">
           <div
             className="modal-dialog modal-dialog-scrollable modal-dialog-centered"
@@ -40,20 +35,16 @@ export default class Modal2 extends Component {
           >
             <div className="modal-content">
               <div className={`modal-header ${style.header}`}>
-                <img
-                  src={image}
-                  alt="Title"
-                  width={300}
-                />
+                <img src={image} alt="Title" width={300} />
               </div>
               <div className="modal-body">
                 <h4>Detail Product</h4>
 
                 <h5>Name</h5>
-                <p >{name}</p>
+                <p>{name}</p>
 
                 <h5>Alias</h5>
-                <p >{alias}</p>
+                <p>{alias}</p>
 
                 <h5>Description</h5>
                 <p>{description}</p>
@@ -63,16 +54,14 @@ export default class Modal2 extends Component {
                 </div>
               </div>
               <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                onClick={() => {
-                  closeModal()
-                }}
-              >
-                Close
-              </button>
-            </div>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={closeModal}
+                >
+                  Close
+                </button>
+              </div>
             </div>
           </div>
         </div>
